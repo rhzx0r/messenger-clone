b0516cf3be77d70484a4ab11a81e8429ff549903
ricardo <ricardo95balam@gmail.com>
2023-07-17 23:13:26 -0600

    final

---
+++ app/components/sidebar/MovileFooter.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import useConversation from "@/app/hooks/useConversation";
+import useRoutes from "@/app/hooks/useRoutes";
+import MovileItem from "./MovileItem";
+
+const MovileFooter = () => {
+  
+  const routes = useRoutes();
+  const { isOpen } = useConversation();
+
+  if (isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="fixed justify-between w-full bottom-0 z-40 flex items-center bg-white border-t-[1px] lg:hidden" >
+      {routes.map((route) => (
+        <MovileItem key={route.href} href={route.href} active={route.active} icon={route.icon} onClick={route.onClick} />
+      ))}
+    </div>
+  );
+}
+ 
+export default MovileFooter;
